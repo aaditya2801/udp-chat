3b603211c941210558d5d590445e792c55560195
aaditya2801 <66722117+aaditya2801@users.noreply.github.com>
2021-05-25 17:23:40 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,40 @@
+import socket 
+import os 
+import threading
+import pyfiglet
+
+
+senderIP = input("Enter Your IP address: ")
+senderPort = 9879
+
+reciverIP = input("Enter Reciver IP address: ")
+reciverPort = 1238
+
+socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+socket.bind((senderIP,senderPort))
+print(pyfiglet.figlet_format("UDP Chat Application", font = "digital" ))
+
+def sendMsg():
+        while True:		
+                message = input()
+                print("\n")
+                if( message == 'bye' or message == 'exit'):    
+                       message = 'Receiver is on Offline Mode.....!!!!'    
+                       socket.sendto(message.encode(),(reciverIP,reciverPort))
+                       os._exit(1)
+                else:
+                     socket.sendto(message.encode(),(reciverIP,reciverPort))
+                    
+def recvMsg():
+        while True:
+                message = socket.recvfrom(100)
+                print("\n\t\t\t\t" + "Receiver -> " + ":" + message[0].decode())
+               
+
+sendMsg = threading.Thread(target=sendMsg)
+recvMsg = threading.Thread(target=recvMsg)
+
+ 
+sendMsg.start()
+recvMsg.start()
